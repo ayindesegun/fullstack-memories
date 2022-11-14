d591e8a3840ba981079117fe30e280b026af0bd5
Samuel Ayinde <ayindesegun17@gmail.com>
2022-11-14 20:35:46 +0100

    I ran somebuild commands

---
+++ client/build/precache-manifest.3a1d57de234f8920b6e2ac60ced31a0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5d567aeb8c50dd5c392088204d0ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0812fe43a4bf522f4341",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0812fe43a4bf522f4341",
+    "url": "/static/js/main.6c9dd5dd.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
